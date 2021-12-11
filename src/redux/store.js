ba154f996fd44d17914733bab59adce3f48c8551
XxAmrHossamxX <amrhossam321@gmail.com>
2021-12-11 08:18:22 +0200

    Updates

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducer"; // Gets the State from the reducer(s)
-
-let store = createStore(rootReducer); // Creates the store from the State received from the reducer(s)
-
-export default store;
